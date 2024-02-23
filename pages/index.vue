102a72797870ec780cb4e24d7db1e3124fd74fba
Masked <trist13579@gmail.com>
2024-02-23 00:30:59 -0500

    Landing

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
 <template>
   <UCard class="rounded-2xl ml-[10%] mr-[10%] min-h-96">
     <div class="flex flex-col place-items-center">
-      <p class="text-xl m-2 text-red-600">This website is currently under construction.</p>
-      <h1 class="text-5xl m-2"> Welcome To <b class="text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-yellow-500">RL Majors</b> 👋</h1>
-      <p class="text-2xl m-2">Join the Discord and start competing!</p>
+      <p class="text-xl m-2 text-red-600">This website is currently under construction</p>
+      <h1 class="text-5xl m-2">The <b class="text-transparent bg-clip-text bg-gradient-to-r from-amber-400 to-yellow-700">Gold Standard</b> For Tournaments <b class="text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-yellow-500">By</b> Rocket League</h1>
+      <h1 class="text-5xl m-2">Players <b class="text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-yellow-500">For</b> Rocket League Players</h1>
+      <p class="text-2xl m-2">Join The Discord And Start Competing</p>
       <div class="flex flex-row m-3">
           <iframe src="https://discord.com/widget?id=1047297400416514068&theme=dark" width="350" height="400" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" class="rounded-2xl"></iframe>
       </div>
