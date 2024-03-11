e1b0388360286608389e58516910a55511d88bc8
Masked <62579894+Masked-Code@users.noreply.github.com>
2024-03-11 13:23:55 -0400

    Update search.vue

--- pages/players/search.vue
+++ pages/players/search.vue
@@ -24,7 +24,7 @@ const platform = ref(platforms[0]);
 const rlid = ref('');
 
 async function searchForPlayer() {
-    const { pending, data: trnPlayerData } = await useLazyFetch(`https://api.tracker.gg/api/v2/rocket-league/standard/profile/${platform}/${rlid}`);
+    const { data: trnPlayerData } = await $fetch(`https://api.tracker.gg/api/v2/rocket-league/standard/profile/${platform}/${rlid}`);
 };
 </script>
 
