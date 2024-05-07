7070bde68d5bdaaab4edd2ac80888dd52c41bd9f
Masked <trist13579@gmail.com>
2024-05-06 23:49:29 -0400

    Fix Player Page working now

--- pages/player/[id].vue
+++ pages/player/[id].vue
@@ -1,6 +1,12 @@
 <template>
   <UCard class="rounded-2xl ml-[10%] mr-[10%] min-h-96">
-    <div v-if="allPlayerData">
+    <div class="m-2">
+      player: {{ currentSelectedPlayer }}
+    </div>
+    <div class="m-2">
+      playerData: {{ currentSelectedPlayerData }}
+    </div>
+    <div>
       <div class="flex flex-flow justify-between">
         <div class="flex flex-row">
           <img src="~/assets/RLMajors_logo_Big.png" class="rounded-2xl max-w-48">
@@ -13,6 +19,20 @@
         </div>
         <div class="flex flex-row">
           <div class="place-self-center m-2 text-lg">
+            <UButton
+              icon="i-heroicons-arrow-top-right-on-square"
+              target="_blank"
+              class="text-primary m-2 text-lg"
+              variant="link"
+              trailing
+            >TRN Tracker</UButton>
+            <UButton
+              icon="i-heroicons-arrow-top-right-on-square"
+              target="_blank"
+              class="text-primary m-2 text-lg"
+              variant="link"
+              trailing
+            >Ballchasing Profile</UButton>
           </div>
         </div>
       </div>
@@ -31,13 +51,11 @@
           </template>
           <template #item="{ item }">
             <div v-if="item.key === 'overview'">
-              <div v-if="playerDataError">
-              Error loading player data
-            </div>
-            <div v-else>
+            <div>
               <div class="flex flex-row">
                 <div class="flex flex-col flex-grow">
                   <UCard class="m-4 flex-grow">
+                    Overview Coming Soon
                     <div class="flex flex-row justify-center">
                       <div class="m-4 flex flex-col justify-center">
                         <img src="~/assets/discord-mark-blue.png" class="rounded-2xl max-w-24">
@@ -56,26 +74,21 @@
             </div>
             </div>
             <div v-if="item.key === 'analytics'">
-              <div v-if="playerDataError">
-              Error loading player data
-            </div>
-            <div v-else>
-              <UCard class="m-4">
+              <UCard class="m-6">
+                <div>
+                Analytis Coming Soon
+                </div>
               </UCard>
-              <UCard class="m-4">
-              </UCard>
-              <UCard class="m-4">
-              </UCard> 
+            <div>
             </div>
             </div>
             <div v-if="item.key === 'matches'">
-              Coming Soon
+              <UCard class="m-6">
+                Mathes Coming Soon
+              </UCard>
             </div>
           </template>
         </UTabs>
-    </div>
-    <div v-else>
-      Error
     </div>
   </UCard>
 </template>
@@ -85,48 +98,58 @@ import PlaytylePieChart from '~/components/PlaystylePieChart.vue'
 import TeamRankingLineChart from '~/components/TeamRankingLineChart.vue'
 const client = useSupabaseClient()
 const route = useRoute()
-const allPlayerData = []
-let currentSelectedSeasonalPlayer = ref()
+const currentSelectedSeason = ref()
+const currentSelectedPlayer = ref()
+const currentSelectedPlayerData = ref()
+let allPlayer = [];
+let allPlayerData = [];
+let seasons = []
 onMounted(async () => {
   const { data: player, error: playerError } = await client
     .from('Players')
     .select('*')
     .eq('discord_username', route.params.id)
     .order('season')
-  if(player) {
-    console.log(player)
+  if (player) {
+    allPlayer = player
+    currentSelectedSeason.value = player[player.length-1].season
+    currentSelectedPlayer.value = player[player.length-1]
+    for (let playerIndex in player) {
+      const { data: playerData, error: playerDataError } = await client
+        .from('Player_Stats')
+        .select('*')
+        .eq('player_uuid', player[playerIndex].uuid)
+        .single()
+      if (playerData) {
+        allPlayerData.push(playerData) 
+      }
+      if (playerDataError) {
+        console.error("playerDataError: ", playerDataError);
+      }
+    }
+    currentSelectedPlayerData.value = allPlayerData[allPlayerData.length-1]
   }
   if (playerError) {
-    console.error(playerError);
+    console.error("playerError: ", playerError);
   }
-  player.forEach(async (foundPlayer) =>{
-    const { data: playerData, error: playerDataError } = await client
-      .from('Player_Stats')
-      .select('*')
-      .eq('player_uuid', foundPlayer.uuid)
-      .order('season')
-    if(playerData) {
-      allPlayerData.push(playerData);
-    }
-    if (playerDataError) {
-      console.error(playerDataError);
-    }
-  })
-  const currentSelectedSeason = ref(player[player.length-1].season)
-  const seasons = []
-  const seasonslist = []
-  console.log(allPlayerData)
-  player.forEach((seasonalData) => seasonslist.push(
+  let seasonEntries = []
+  player.forEach((eachPlayerSeason) => seasonEntries.push(
     {
-      label: `Season ${seasonalData.season}`, 
-      click: () => {currentSelectedSeason.value = seasonalData.season}
+      label: `Season ${eachPlayerSeason.season}`, 
+      click: () => {currentSelectedSeason.value = eachPlayerSeason.season}
     }
   ));
-  seasons.push(seasonslist)
-  currentSelectedSeasonalPlayer.value = computed(() => {
-    console.log("test", player.find((foundPlayer) => foundPlayer.season == currentSelectedSeason.value))
-    return player.find((foundPlayer) => foundPlayer.season == currentSelectedSeason.value)
-  })
+  seasons.push(seasonEntries)
+})
+currentSelectedPlayer.value = computed(() => {
+  return allPlayer.find((playah) => playah.season == currentSelectedSeason.value)
+})
+currentSelectedPlayerData.value = computed(() => {
+  return allPlayerData.find((playahData) => playahData.season == currentSelectedSeason.value)
+})
+watch(currentSelectedSeason, async (newVal) => {
+  currentSelectedPlayer.value = allPlayer.find((playah) => playah.season == newVal)
+  currentSelectedPlayerData.value = allPlayerData.find((playahData) => playahData.season == newVal)
 })
 const tabs = [{
   key: 'overview',
@@ -140,7 +163,7 @@ const tabs = [{
   description: 'View analytics here.'
 }, {
   key: 'matches',
-  label: 'Recent Matches',
+  label: 'Matches',
   icon: 'i-heroicons-list-bullet',
   description: 'View recent matches.'
 }]
