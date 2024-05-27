8715b0621cd7daa0606c2dddcdb7fc49aeb9692f
Masked <trist13579@gmail.com>
2024-05-27 15:49:08 -0400

    Starting to correct the leaderboards page

--- pages/leaderboards.vue
+++ pages/leaderboards.vue
@@ -1,300 +1,37 @@
 <template>
   <UCard class="rounded-2xl ml-[10%] mr-[10%] min-h-96">
+    <UDropdown :items="seasons" :popper="{ placement: 'bottom-start' }">
+      <UButton color="primary" trailing-icon="i-heroicons-chevron-down-20-solid" class="m-1" size="md" >
+        Season {{ currentSelectedSeason }}
+      </UButton>
+    </UDropdown>
     <UDropdown :items="divisions" :popper="{ placement: 'bottom-start' }">
       <UButton color="primary" trailing-icon="i-heroicons-chevron-down-20-solid" class="m-1" size="md" >
-        {{ currentSelectedDivision }}
+        Div {{ currentSelectedDivision }}
       </UButton>
     </UDropdown>
       <div class="flex flex-col place-items-center">
-        <UTable v-if="currentSelectedDivision == 'Div 1'" :empty-state="{ icon: 'i-heroicons-circle-stack-20-solid', label: 'No Team Data.' }" :rows="d1TeamData || undefined" :columns="columns" class="w-full" >
+        <UTable :empty-state="{ icon: 'i-heroicons-circle-stack-20-solid', label: 'No Team Data.' }" :rows="teamData" :columns="columns" class="w-full" >
           <template #team_name-header="{column}">
-            <div class="flex flex-col place-items-center">
-              {{ column.label }}
-            </div>
+            {{ column.label }}
           </template>
-          <template #team_name-data="{row}">
-            <div class="flex flex-col place-items-center">
-              <ULink
-              :to="`/teams/div1/${row.team_name.split(' ').join('')}`"
-                class="text-primary font-bold text-lg"
-              >
-              {{ row.team_name }}
-            </ULink>
-            </div>
+          <template #name-data="{row}">
+            {{ row.name }}
           </template>
-          <template #franchise_owner-header="{column}">
-            <div class="flex flex-col place-items-center">
-              {{ column.label }}
-            </div>
+          <template #owner-header="{column}">
+            {{ column.label }}
           </template>
-          <template #franchise_owner-data="{row}">
-            <div class="flex flex-col place-items-center">
-              {{ row.franchise_owner }}
-            </div>
+          <template #owner-data="{row}">
+            {{ row.owner }}
           </template>
-          <template #team_captain-header="{column}">
-            <div class="flex flex-col place-items-center">
-              {{ column.label }}
-            </div>
+          <template #captain-data="{row}">
+            {{ row.captain }}
           </template>
-          <template #team_captain-data="{row}">
-            <div class="flex flex-col place-items-center">
-              {{ row.team_captain }}
-            </div>
+          <template #salary-data="{row}">
+            {{ row.salary }}
           </template>
-          <template #non_captain_players-header="{column}">
-            <div class="flex flex-col place-items-center">
-              {{ column.label }}
-            </div>
-          </template>
-          <template #non_captain_players-data="{row}">
-            <div class="flex flex-col place-items-center">
-              <p v-for="singlePlayer in row.non_captain_players.ncp">{{ singlePlayer }}</p>
-            </div>
-          </template>
-          <template #current_salary-header="{column}">
-            <div class="flex flex-col place-items-center">
-              {{ column.label }}
-            </div>
-          </template>
-          <template #current_salary-data="{row}">
-            <div class="flex flex-col place-items-center">
-              {{ row.current_salary }}
-            </div>
-          </template>
-          <template #remaining_transactions-header="{column}">
-            <div class="flex flex-col place-items-center">
-              {{ column.label }}
-            </div>
-          </template>
-          <template #remaining_transactions-data="{row}">
-            <div class="flex flex-col place-items-center">
-              {{ row.remaining_transactions }}
-            </div>
-          </template>
-          <template #wins-header="{column}">
-            <div class="flex flex-col place-items-center">
-              {{ column.label }}
-            </div>
-          </template>
-          <template #wins-data="{row}">
-            <div class="flex flex-col place-items-center text-green-600 font-bold text-lg">
-              {{ row.wins }}
-            </div>
-          </template>
-          <template #losses-header="{column}">
-            <div class="flex flex-col place-items-center">
-              {{ column.label }}
-            </div>
-          </template>
-          <template #losses-data="{row}">
-            <div class="flex flex-col place-items-center text-red-600 font-bold text-lg">
-              {{ row.losses }}
-            </div>
-          </template>
-          <template #goal_difference-header="{column}">
-            <div class="flex flex-col place-items-center">
-              {{ column.label }}
-            </div>
-          </template>
-          <template #goal_difference-data="{row}">
-            <div class="flex flex-col place-items-center text-primary font-bold text-lg">
-              {{ row.goal_difference }}
-            </div>
-          </template>
-        </UTable>
-        <UTable v-if="currentSelectedDivision == 'Div 2'" :empty-state="{ icon: 'i-heroicons-circle-stack-20-solid', label: 'No Team Data.' }" :rows="d2TeamData || undefined" :columns="columns" class="w-full" >
-          <template #team_name-header="{column}">
-            <div class="flex flex-col place-items-center">
-              {{ column.label }}
-            </div>
-          </template>
-          <template #team_name-data="{row}">
-            <div class="flex flex-col place-items-center">
-              <ULink
-              :to="`/teams/div2/${row.team_name.split(' ').join('')}`"
-                class="text-primary font-bold text-lg"
-              >
-              {{ row.team_name }}
-            </ULink>
-            </div>
-          </template>
-          <template #franchise_owner-header="{column}">
-            <div class="flex flex-col place-items-center">
-              {{ column.label }}
-            </div>
-          </template>
-          <template #franchise_owner-data="{row}">
-            <div class="flex flex-col place-items-center">
-              {{ row.franchise_owner }}
-            </div>
-          </template>
-          <template #team_captain-header="{column}">
-            <div class="flex flex-col place-items-center">
-              {{ column.label }}
-            </div>
-          </template>
-          <template #team_captain-data="{row}">
-            <div class="flex flex-col place-items-center">
-              {{ row.team_captain }}
-            </div>
-          </template>
-          <template #non_captain_players-header="{column}">
-            <div class="flex flex-col place-items-center">
-              {{ column.label }}
-            </div>
-          </template>
-          <template #non_captain_players-data="{row}">
-            <div class="flex flex-col place-items-center">
-              <p v-for="singlePlayer in row.non_captain_players.ncp">{{ singlePlayer }}</p>
-            </div>
-          </template>
-          <template #current_salary-header="{column}">
-            <div class="flex flex-col place-items-center">
-              {{ column.label }}
-            </div>
-          </template>
-          <template #current_salary-data="{row}">
-            <div class="flex flex-col place-items-center">
-              {{ row.current_salary }}
-            </div>
-          </template>
-          <template #remaining_transactions-header="{column}">
-            <div class="flex flex-col place-items-center">
-              {{ column.label }}
-            </div>
-          </template>
-          <template #remaining_transactions-data="{row}">
-            <div class="flex flex-col place-items-center">
-              {{ row.remaining_transactions }}
-            </div>
-          </template>
-          <template #wins-header="{column}">
-            <div class="flex flex-col place-items-center">
-              {{ column.label }}
-            </div>
-          </template>
-          <template #wins-data="{row}">
-            <div class="flex flex-col place-items-center text-green-600 font-bold text-lg">
-              {{ row.wins }}
-            </div>
-          </template>
-          <template #losses-header="{column}">
-            <div class="flex flex-col place-items-center">
-              {{ column.label }}
-            </div>
-          </template>
-          <template #losses-data="{row}">
-            <div class="flex flex-col place-items-center text-red-600 font-bold text-lg">
-              {{ row.losses }}
-            </div>
-          </template>
-          <template #goal_difference-header="{column}">
-            <div class="flex flex-col place-items-center">
-              {{ column.label }}
-            </div>
-          </template>
-          <template #goal_difference-data="{row}">
-            <div class="flex flex-col place-items-center text-primary font-bold text-lg">
-              {{ row.goal_difference }}
-            </div>
-          </template>
-        </UTable>
-        <UTable v-if="currentSelectedDivision == 'Div 3'" :empty-state="{ icon: 'i-heroicons-circle-stack-20-solid', label: 'No Team Data.' }" :rows="d3TeamData || undefined" :columns="columns" class="w-full" >
-          <template #team_name-header="{column}">
-            <div class="flex flex-col place-items-center">
-              {{ column.label }}
-            </div>
-          </template>
-          <template #team_name-data="{row}">
-            <div class="flex flex-col place-items-center">
-              <ULink
-              :to="`/teams/div3/${row.team_name.split(' ').join('')}`"
-                class="text-primary font-bold text-lg"
-              >
-              {{ row.team_name }}
-            </ULink>
-            </div>
-          </template>
-          <template #franchise_owner-header="{column}">
-            <div class="flex flex-col place-items-center">
-              {{ column.label }}
-            </div>
-          </template>
-          <template #franchise_owner-data="{row}">
-            <div class="flex flex-col place-items-center">
-              {{ row.franchise_owner }}
-            </div>
-          </template>
-          <template #team_captain-header="{column}">
-            <div class="flex flex-col place-items-center">
-              {{ column.label }}
-            </div>
-          </template>
-          <template #team_captain-data="{row}">
-            <div class="flex flex-col place-items-center">
-              {{ row.team_captain }}
-            </div>
-          </template>
-          <template #non_captain_players-header="{column}">
-            <div class="flex flex-col place-items-center">
-              {{ column.label }}
-            </div>
-          </template>
-          <template #non_captain_players-data="{row}">
-            <div class="flex flex-col place-items-center">
-              <p v-for="singlePlayer in row.non_captain_players.ncp">{{ singlePlayer }}</p>
-            </div>
-          </template>
-          <template #current_salary-header="{column}">
-            <div class="flex flex-col place-items-center">
-              {{ column.label }}
-            </div>
-          </template>
-          <template #current_salary-data="{row}">
-            <div class="flex flex-col place-items-center">
-              {{ row.current_salary }}
-            </div>
-          </template>
-          <template #remaining_transactions-header="{column}">
-            <div class="flex flex-col place-items-center">
-              {{ column.label }}
-            </div>
-          </template>
-          <template #remaining_transactions-data="{row}">
-            <div class="flex flex-col place-items-center">
-              {{ row.remaining_transactions }}
-            </div>
-          </template>
-          <template #wins-header="{column}">
-            <div class="flex flex-col place-items-center">
-              {{ column.label }}
-            </div>
-          </template>
-          <template #wins-data="{row}">
-            <div class="flex flex-col place-items-center text-green-600 font-bold text-lg">
-              {{ row.wins }}
-            </div>
-          </template>
-          <template #losses-header="{column}">
-            <div class="flex flex-col place-items-center">
-              {{ column.label }}
-            </div>
-          </template>
-          <template #losses-data="{row}">
-            <div class="flex flex-col place-items-center text-red-600 font-bold text-lg">
-              {{ row.losses }}
-            </div>
-          </template>
-          <template #goal_difference-header="{column}">
-            <div class="flex flex-col place-items-center">
-              {{ column.label }}
-            </div>
-          </template>
-          <template #goal_difference-data="{row}">
-            <div class="flex flex-col place-items-center text-primary font-bold text-lg">
-              {{ row.goal_difference }}
-            </div>
+          <template #transactions-data="{row}">
+            {{ row.transactions }}
           </template>
         </UTable>
       </div>
@@ -302,71 +39,84 @@
 </template>
 
 <script lang="ts" setup>
-let currentSelectedDivision = ref('Div 1')
 const client = useSupabaseClient()
+let currentSelectedSeason = ref(1)
+let currentSelectedDivision = ref(1)
+let teamData = ref();
+onMounted(async () => {
+  const { data: initTeamData } = await client
+    .from('Teams')
+    .select('*')
+    .eq('season', 1)
+    .eq('division', 1)
+    teamData.value = initTeamData;
+})
 
-const { data: d1TeamData } = await client
-  .from('S1_Team_Data')
-  .select('')
-  .eq('division', 1)
-  .order('wins', { ascending: false })
-  .order('losses', { ascending: true })
-  .order('goal_difference', { ascending: false})
+watch(currentSelectedSeason, async (newVal) => {
+  const { data: newTeamData } = await client
+    .from('Teams')
+    .select('*')
+    .eq('division', currentSelectedDivision.value)
+    .eq('season', newVal)
+    teamData.value = newTeamData;
+})
 
-const { data: d2TeamData } = await client
-.from('S1_Team_Data')
-  .select('')
-  .eq('division', 2)
-  .order('wins', { ascending: false })
-  .order('losses', { ascending: true })
-  .order('goal_difference', { ascending: false})
-  
-const { data: d3TeamData } = await client
-.from('S1_Team_Data')
-  .select('')
-  .eq('division', 3)
-  .order('wins', { ascending: false })
-  .order('losses', { ascending: true })
-  .order('goal_difference', { ascending: false})
-  
+watch(currentSelectedDivision, async (newVal) => {
+  const { data: newTeamData } = await client
+    .from('Teams')
+    .select('*')
+    .eq('division', newVal)
+    .eq('season', currentSelectedSeason.value)
+    teamData.value = newTeamData;
+})
+
 const columns = [{
-    key: 'team_name',
+    key: 'name',
     label: 'Team Name'
   }, {
-    key: 'franchise_owner',
+    key: 'owner',
     label: 'Franchise Owner'
   }, {
-    key: 'current_salary',
+    key: 'salary',
     label: 'Remaining Salary'
   }, {
-    key: 'remaining_transactions',
+    key: 'transactions',
     label: 'Remaining Transactions'
-  }, {
-    key: 'wins',
-    label: 'Wins'
-  }, {
-    key: 'losses',
-    label: 'Losses'
-  }, {
-    key: 'goal_difference',
-    label: 'Goal Difference'
   }]
 
+const seasons = [
+  [{
+    label: 'Season 1',
+    click: () => {
+      currentSelectedSeason.value = 1
+    }
+  }, {
+    label: 'Season 2',
+    click: () => {
+      currentSelectedSeason.value = 2
+    }
+  }, {
+    label: 'Season 3',
+    click: () => {
+      currentSelectedSeason.value = 3
+    }
+  }]
+]
 const divisions = [
   [{
     label: 'Div 1',
     click: () => {
-      currentSelectedDivision.value = 'Div 1'
+      currentSelectedDivision.value = 1
     }
   }, {
     label: 'Div 2',
     click: () => {
-      currentSelectedDivision.value = 'Div 2'
+      currentSelectedDivision.value = 2
     }
   }, {
     label: 'Div 3',
     click: () => {
-      currentSelectedDivision.value = 'Div 3'
+      currentSelectedDivision.value = 3
     }
   }]
 ]
