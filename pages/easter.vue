9945437a9d40f543939067428c6b43d49299723d
Masked <62579894+Masked-Code@users.noreply.github.com>
2024-06-12 23:30:08 -0400

    Update easter.vue

--- pages/easter.vue
+++ pages/easter.vue
@@ -2,7 +2,7 @@
   <UCard class="rounded-2xl ml-[10%] mr-[10%] min-h-96">
   <div class="flex flex-col place-items-center">
     <h1 class="text-7xl m-2">🎊</h1>
-    <h1 class="text-5xl text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-orange-500 p-5">Mebu Is The Most Overrated Player</h1>
+    <h1 class="text-5xl text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-orange-500 p-5">bozo is a 🤡</h1>
     <h1 class="text-7xl m-2">🥳</h1>
   </div>
 </UCard>
